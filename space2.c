4c924eb387a725a50ed5e513501a448c1d7ac5ef
danielcanenciagarcia <danik3377@outlook.com>
2017-02-13 14:57:16 +0100

    Add files via upload

---
+++ space2.c
@@ -0,0 +1,84 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "object.h"
+
+
+struct _Object{
+	
+	Id id;
+	char  name[WORD_SIZE + 1];
+	BOOL player;
+	
+};
+
+Object* object_create(Id id){
+	
+	Object* newObject=NULL;
+	
+	if(id=NO_ID)
+		return NULL;
+		
+	newObject = (Object *) malloc(sizeof (Object));
+	
+	if(!newObject)
+		return NULL;
+		
+	newObject->id = id;
+	newObject->back;
+	newObject->next;
+	newObject->player;
+	
+	return newObject;
+	
+}
+
+STATUS object_destroy(Object* object){
+	
+	if(!object)
+		return ERROR;
+	
+	free(object);
+	
+	object = NULL;
+	
+	return OK;
+	
+}
+
+
+STATUS object_set_name(Object* object, char* name){
+	
+	if (!object || !name)
+        return ERROR;
+    
+
+    if (!strcpy(object->name, name)
+        return ERROR;
+    
+
+    return OK;
+	
+}
+
+
+const char * object_get_name(Object* object){
+	
+	if(!object) 
+        return NULL;
+    
+    return object->name;
+	
+}
+
+
+
+
+STATUS object_print(Object* object){
+	
+	
+	
+}
+
+	
+	
